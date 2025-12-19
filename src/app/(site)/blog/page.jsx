73285885fb4464795183bd6229466898be2e73f1
vasan-sri45 <srinivasanaadhi123@gmail.com>
2025-12-19 13:31:07 +0530

    page.jsx

--- src/app/(site)/blog/page.jsx
+++ src/app/(site)/blog/page.jsx
@@ -16,8 +16,8 @@ const BlogPage = () => {
         title='Blog'
         description='Discover a wealth of insightful materials meticulously crafted to provide you with a comprehensive understanding of the latest trends.'
         breadcrumbLinks={breadcrumbLinks}
-      />
-      <BlogList />
+      /> 
+      {/* <BlogList /> */}
     </>
   );
 };
